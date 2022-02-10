7b530dc002305a5131beef0aed05f6ce6d6916f1
Spence777 <66566699+Spence777@users.noreply.github.com>
2022-02-10 19:20:49 +0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+<footer class="p-5 bg-dark text-white text-center position-relative">
+  <div class="container">
+    
+    <p class="lead">Copyright &copy; <?php echo date('Y'); ?> | CCMES | All Rights Reserved.</p>
+    <a href="#" class="position-absolute bottom-0 end-0 p-5"><i class="fas fa-arrow-up"></i></a>
+
+  </div>
+</footer>
+
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
